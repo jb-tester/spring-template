e68c3a44d035fc796642d72ed72b34c68df0da1d
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2020-04-22 14:20:18 +0300

    Initial commit

---
+++ src/main/java/com/mytests/spring/configs/MyConfig.java
@@ -0,0 +1,15 @@
+package com.mytests.spring.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * *******************************
+ * <p>Created by irina on 22.04.2020.</p>
+ * <p>Project: spring-template</p>
+ * *******************************
+ */
+@Configuration
+@ComponentScan("com.mytests.spring")
+public class MyConfig {
+}
